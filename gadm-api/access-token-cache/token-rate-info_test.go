05cac7479b9266ff7cc14dd8230d7a0fc8eb04db
christopher z <krzysztof.zinger@gmail.com>
2025-07-16 07:59:58 +0200

    add handleHit expired token test

--- gadm-api/access-token-cache/token-rate-info_test.go
+++ gadm-api/access-token-cache/token-rate-info_test.go
@@ -14,7 +14,7 @@ type DateInfo struct {
 func getCreatedAtFromDateInfo(dateInfo DateInfo) time.Time {
 	return time.Now().AddDate(dateInfo.year, dateInfo.month, dateInfo.day)
 }
-func TestHandleHitExpiredToken(t *testing.T) {
+func TestIsTokenExpiredToken(t *testing.T) {
 	t.Logf("Test: isTokenExpired")
 
 	expiredDateInfos := []DateInfo{
@@ -41,6 +41,30 @@ func TestHandleHitExpiredToken(t *testing.T) {
 		if isTokenExpired(getCreatedAtFromDateInfo(v)) {
 			t.Errorf("valid token date evaluated as expired: date: Y%d M%d D%d",
 				v.year, v.month, v.day)
+		}
+	}
+}
+
+func TestHandleHitExpiredToken(t *testing.T) {
+	t.Logf("Test: TokenRateInfo.handleHit - expired token")
+
+	expiredDateInfos := []DateInfo{
+		{year: 0, month: -3, day: -1},
+		{year: 0, month: -4, day: 0},
+		{year: -1, month: 0, day: 0},
+	}
+
+	for _, expiredDate := range expiredDateInfos {
+		tokenRateInfo := newTokenRateInfo(getCreatedAtFromDateInfo(expiredDate))
+		err := tokenRateInfo.handleHit()
+		if err == nil {
+			t.Errorf(
+				"expected expired token error for creation date: Y%d M%d D%d",
+				expiredDate.year, expiredDate.month, expiredDate.day)
+		} else if err.Error() != TokenExpiredMsg {
+			t.Errorf(
+				"unexpected error message: '%s'. Expected '%s'",
+				err.Error(), TokenExpiredMsg)
 		}
 	}
 }
