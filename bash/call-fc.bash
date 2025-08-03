b4446aa2266868221b797b48ad8f87f87f21a802
christopher z <krzysztof.zinger@gmail.com>
2025-08-03 15:28:21 +0200

    refine test script

--- bash/call-fc.bash
+++ bash/call-fc.bash
@@ -1,9 +1,9 @@
-
+TOKEN=
 # page 1
- curl  "localhost:8080/api/v1/fc/lv3?page-size=10&start-at=0&gid-0=POL" | tee geojsonl.json 
+ curl -H "Authorization: Bearer $TOKEN" "localhost:8080/api/v1/fc/lv3?page-size=10&start-at=0&gid-0=POL" | tee geojsonl.json 
 
 # page 2
-curl  "localhost:8080/api/v1/fc/lv3?gid-0=POL&page-size=10&start-at=108435" | tee geojsonl_2.json 
+curl -H "Authorization: Bearer $TOKEN" "localhost:8080/api/v1/fc/lv3?gid-0=POL&page-size=10&start-at=108435" | tee geojsonl_2.json 
 
 # page 3
 curl  "localhost:8080/api/v1/fc/lv3?gid-0=POL&page-size=10&start-at=108445" | tee geojsonl_3.json 
