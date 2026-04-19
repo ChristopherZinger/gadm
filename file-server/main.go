5b359664dd0535a55cea87511096486079631d31
christopher z <krzysztof.zinger@gmail.com>
2026-04-19 10:13:15 +0200

    add simple file server for pm tiles

---
+++ file-server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+	"os"
+)
+
+func withCORS(h http.Handler) http.Handler {
+	allowOrigin := os.Getenv("CORS_ORIGIN")
+	if allowOrigin == "" {
+		allowOrigin = "*"
+	}
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", allowOrigin)
+		w.Header().Set("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Range, Accept")
+		w.Header().Set("Access-Control-Expose-Headers", "Content-Length, Content-Range, Accept-Ranges, ETag, Cache-Control")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		h.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("public"))
+	http.Handle("/", withCORS(fs))
+	http.ListenAndServe(":8080", nil)
+}
