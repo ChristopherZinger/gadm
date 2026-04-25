5eebbe181afe30067b75fdd8e856b769e9020d98
christopher z <krzysztof.zinger@gmail.com>
2026-04-25 09:48:36 +0200

    extract AdmDetail component and add copy and docs link to side panel

---
+++ webapp/src/lib/components/AdmDetails.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	let { info }: { info: { properties: Record<string, unknown> } } = $props();
+</script>
+
+<div>
+	<h1 class="text-2xl font-bold">{info.properties.country}</h1>
+	<table class="mt-4">
+		<thead>
+			<tr class="border-b border-gray-200">
+				<th class="pr-2 text-left font-semibold">info</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each Object.entries(info.properties) as [key, value] (key)}
+				<tr class="border-b border-gray-200">
+					<td class="pr-2">{key.toLowerCase().replaceAll('_', ' ')}</td>
+					<td class="p-2">{value}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
--- webapp/src/routes/+page.svelte
+++ webapp/src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import AdmDetails from '$lib/components/AdmDetails.svelte';
 	import Map from '$lib/components/map/Map.svelte';
 	import { mapSelection } from '$lib/stores/map-selection';
 </script>
@@ -12,24 +13,23 @@
 		</div>
 	</div>
 	<div class="side-panel">
-		{#if $mapSelection}
-			<h1 class="text-2xl font-bold">{$mapSelection.properties.country}</h1>
-			<table class="mt-4">
-				<thead>
-					<tr class="border-b border-gray-200">
-						<th class="pr-2 text-left font-semibold">info</th>
-					</tr>
-				</thead>
-				<tbody>
-					{#each Object.entries($mapSelection.properties) as [key, value] (key)}
-						<tr class="border-b border-gray-200">
-							<td class="pr-2">{key.toLowerCase().replace('_', ' ')}</td>
-							<td class="p-2">{value}</td>
-						</tr>
-					{/each}
-				</tbody>
-			</table>
-		{/if}
+		<div>
+			<div>
+				<h1 class="text-2xl font-bold">Hello World Lines!</h1>
+				<p>
+					Explore worldwide administrative boundaries on an interactive map. Click anywhere on the
+					map to pick a unit and see its details here in the side panel.
+				</p>
+			</div>
+			{#if $mapSelection && $mapSelection.type === 'adm'}
+				<div class="py-4">
+					<AdmDetails info={$mapSelection} />
+				</div>
+			{/if}
+		</div>
+		<div>
+			<a class="underline" href="https://docs.worldlines.dev" target="_blank">API docs</a>
+		</div>
 	</div>
 </div>
 
@@ -38,5 +38,9 @@
 		width: 500px;
 		margin: 10px;
 		padding: 10px;
+		display: flex;
+		flex-direction: column;
+		gap: 10px;
+		justify-content: space-between;
 	}
 </style>
