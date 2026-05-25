9f656021087feb416784c322948bc4661ad373c9
christopher z <krzysztof.zinger@gmail.com>
2026-05-25 10:38:52 +0200

    refine bash script

--- gadm-api/run_cron_job.sh
+++ gadm-api/run_cron_job.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cd "$(dirname "$0")/main"
+cd "$(dirname "$0")/.."
 
-go run .
+docker compose build gadm-api
+
+docker compose run --rm -e SERVICE_TYPE=cron_job gadm-api
