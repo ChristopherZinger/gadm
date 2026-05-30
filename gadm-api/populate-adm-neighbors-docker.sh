4cbc075fe22ea8f2f7bf27a35e7a1fbbfb214ae8
christopher z <krzysztof.zinger@gmail.com>
2026-05-30 13:36:07 +0200

    keep container after it's done

--- gadm-api/populate-adm-neighbors-docker.sh
+++ gadm-api/populate-adm-neighbors-docker.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${SCRIPT_DIR}/.."
 
-exec docker compose run -d --rm --build \
+exec docker compose run -d --build \
   -e SERVICE_TYPE=cron_job \
   -e CRON_JOB_NAME=populate_adm_neighbors \
   gadm-api | tee pop-neighbors-detached.log
