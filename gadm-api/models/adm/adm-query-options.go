1475a6851cbd09f746943cbc68849353adc4b08b
christopher z <krzysztof.zinger@gmail.com>
2026-06-10 06:54:04 +0200

    refactor adm query builder

--- gadm-api/models/adm/adm-query-options.go
+++ gadm-api/models/adm/adm-query-options.go
@@ -1,5 +1,7 @@
 package adm
 
+import "gadm-api/utils"
+
 type admQueryOpts struct {
 	lv              *int
 	startAfterFid   *string
@@ -20,23 +22,38 @@ func NewAdmQueryOptsBuilder() *admQueryOptsBuilder {
 	}}
 }
 
-func (builder *admQueryOptsBuilder) SetLv(lv int) *admQueryOptsBuilder {
-	builder.conf.lv = &lv
-	return builder
-}
-
 func (builder *admQueryOptsBuilder) SetStartAfterId(startAfterId string) *admQueryOptsBuilder {
+	if startAfterId == "" {
+		return builder
+	}
+
 	builder.conf.startAfterId = &startAfterId
 	return builder
 }
 
 func (builder *admQueryOptsBuilder) SetStartAfterFid(startAfterFid string) *admQueryOptsBuilder {
+	if startAfterFid == "" {
+		return builder
+	}
 	builder.conf.startAfterFid = &startAfterFid
 	return builder
 }
 
-func (builder *admQueryOptsBuilder) SetBatchSize(batchSize *int) *admQueryOptsBuilder {
-	builder.conf.batchSize = batchSize
+func (builder *admQueryOptsBuilder) SetLvAndBatchSize(lv *int, batchSize *int) *admQueryOptsBuilder {
+	builder.conf.lv = lv
+
+	_batchSize := *batchSize
+	if lv != nil {
+		if *lv < 2 {
+			_batchSize = utils.Clamp(_batchSize, 1, 5)
+		} else if *lv < 4 {
+			_batchSize = utils.Clamp(_batchSize, 1, 20)
+		} else {
+			_batchSize = utils.Clamp(_batchSize, 1, 50)
+		}
+	}
+
+	builder.conf.batchSize = &_batchSize
 	return builder
 }
 
--- gadm-api/models/adm/http.go
+++ gadm-api/models/adm/http.go
@@ -116,78 +116,59 @@ func getPointFromRequestBody(r *http.Request) (utils.Point, error) {
 	return utils.NewPointLngLat(geometry.Point[0], geometry.Point[1]), nil
 }
 
-func (handler *Handler) validateAdmFcQueryParams(urlQuery url.Values) (admQueryOpts, error) {
-	startAfterId := urlQuery.Get("start-after-id")
-	startAfterFid := urlQuery.Get("start-after-fid")
-	batchSize := urlQuery.Get("batch-size")
-	lvString := urlQuery.Get("lv")
-
-	getLvInt := func(lv string) (int, error) {
-		_lv, err := strconv.Atoi(lv)
-		if err != nil {
-			return 0, fmt.Errorf("failed_int_conversion %v", err)
-		}
-		if _lv < 0 || _lv > 5 {
-			return 0, fmt.Errorf("range_error %v", err)
-		}
-		return _lv, nil
+func getLevelIntFromString(level string) (*int, error) {
+	if level == "" {
+		return nil, nil
 	}
-	var _lv *int
-	if lvString != "" {
-		__lv, err := getLvInt(lvString)
-		if err != nil {
-			return admQueryOpts{}, fmt.Errorf("failed_parsing_query_param_lv: %v", err)
-		}
-		_lv = &__lv
-	}
-
-	var _batchSize int
-	var err error
-	getBatchSizeForLv := func(lv int, batchSize int) int {
-		if lv < 2 {
-			return utils.Clamp(batchSize, 1, 5)
-		}
-		if lv < 4 {
-			return utils.Clamp(batchSize, 1, 20)
-		}
-		return utils.Clamp(batchSize, 1, 50)
-	}
-	if batchSize != "" {
-		_batchSize, err = strconv.Atoi(batchSize)
-		if err != nil {
-			return admQueryOpts{}, fmt.Errorf("failed_int_conversion batch_size %v", err)
-		}
-		if _lv != nil {
-			_batchSize = getBatchSizeForLv(*_lv, _batchSize)
-		} else {
-			_batchSize = utils.Clamp(_batchSize, 1, 20)
-		}
-	}
-
-	optsBuilder := NewAdmQueryOptsBuilder()
-	optsBuilder.SetBatchSize(&_batchSize)
-	optsBuilder.SetIncludeGeometry(true)
-	if _lv != nil {
-		optsBuilder.SetLv(*_lv)
-	}
-	if startAfterId != "" {
-		optsBuilder.SetStartAfterId(startAfterId)
-	}
-	if startAfterFid != "" {
-		optsBuilder.SetStartAfterFid(startAfterFid)
-	}
-
-	opts, err := optsBuilder.Build()
+	_lv, err := strconv.Atoi(level)
 	if err != nil {
-		return admQueryOpts{}, fmt.Errorf("failed_to_build_adm_query_opts %v", err)
+		return &_lv, fmt.Errorf("failed_converting_level_to_int %v", err)
 	}
-	return opts, nil
+	if _lv < 0 || _lv > 5 {
+		return &_lv, fmt.Errorf("level_range_error %v", err)
+	}
+	return &_lv, nil
+}
+
+func getBatchSizeIntFromString(batchSize string) (*int, error) {
+	if batchSize == "" {
+		return nil, nil
+	}
+	_batchSize, err := strconv.Atoi(batchSize)
+	if err != nil {
+		return &_batchSize, fmt.Errorf("failed_converting_batch_size_to_int %v", err)
+	}
+	return &_batchSize, nil
 }
 
 func (handler *Handler) GetAdmFeatureCollectionHandler(w http.ResponseWriter, r *http.Request, baseUrl url.URL) {
-	opts, err := handler.validateAdmFcQueryParams(r.URL.Query())
+	startAfterId := r.URL.Query().Get("start-after-id")
+	startAfterFid := r.URL.Query().Get("start-after-fid")
+	batchSize := r.URL.Query().Get("batch-size")
+	lvString := r.URL.Query().Get("lv")
+
+	_lv, err := getLevelIntFromString(lvString)
 	if err != nil {
-		logger.Error("failed_to_validate_adm_fc_query_params %v", err)
+		logger.Error("failed_parsing_query_param_lv: %v", err)
+		http.Error(w, "invalid_request", http.StatusBadRequest)
+		return
+	}
+
+	_batchSize, err := getBatchSizeIntFromString(batchSize)
+	if err != nil {
+		logger.Error("failed_parsing_query_param_batch_size: %v", err)
+		http.Error(w, "invalid_request", http.StatusBadRequest)
+		return
+	}
+
+	optsBuilder := NewAdmQueryOptsBuilder()
+	optsBuilder.SetLvAndBatchSize(_lv, _batchSize)
+	optsBuilder.SetStartAfterId(startAfterId)
+	optsBuilder.SetStartAfterFid(startAfterFid)
+	optsBuilder.SetIncludeGeometry(true)
+	opts, err := optsBuilder.Build()
+	if err != nil {
+		logger.Error("failed_to_build_adm_query_opts %v", err)
 		http.Error(w, "invalid_request", http.StatusBadRequest)
 		return
 	}
